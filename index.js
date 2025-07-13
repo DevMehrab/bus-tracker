0aebd010a36d1eef02a62f8ed37009af8bc54350
Mehrab Hossain <88287873+DevMehrab@users.noreply.github.com>
2025-07-14 02:41:35 +0600

    Update index.js

--- index.js
+++ index.js
@@ -16,6 +16,7 @@ const LOCATION_POSTED = "location-posted";
 const LOCATION_UPDATED = "location-updated";
 
 app.set("view engine", "ejs");
+app.set('views', path.join(__dirname, 'views'));
 // Serve a basic message at root
 app.get("/", (req, res) => {
   res.render("index", {

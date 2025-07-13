9f95f11929627666f7129b10e3563fe845f75c3b
Mehrab Hossain <dev.mehrabhossain@gmail.com>
2025-07-14 00:45:51 +0600

    initial commit

---
+++ server.js
@@ -0,0 +1,42 @@
+// server.js
+
+const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+  },
+});
+
+const LOCATION_POSTED = "location-posted";
+const LOCATION_UPDATED = "location-updated";
+
+// Serve a basic message at root
+app.get("/", (req, res) => {
+  res.send("Socket.IO server is running!");
+});
+
+// Handle socket connections
+io.on("connection", (socket) => {
+  console.log("A user connected:", socket.id);
+
+  socket.on(LOCATION_POSTED, (locationPayload) => {
+    console.log("Message received:", locationPayload);
+    // Broadcast the message to all other clients
+    socket.broadcast.emit(LOCATION_UPDATED, locationPayload);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("User disconnected:", socket.id);
+  });
+});
+
+// Start the server
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+});
